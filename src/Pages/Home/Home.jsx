bee10e3edcd2dba93f9f7d466c23f123b329a9aa
saketkum11 <kumarsaket601@gmail.com>
2022-05-16 01:51:22 +0530

    added:html and css

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -30,6 +30,29 @@ const Home = () => {
           </div>
         </div>
       </nav>
+      <aside className="mt-5">
+        <div className="row no-gutters position-relative">
+          <div className="col-md-6 mb-md-0 p-md-4">
+            <img
+              src="../assets/question.jpg "
+              className=" mr-3 float-right img-thumbnail w-50 img-fluid  shadow-sm"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-6 position-static p-4 pl-md-0  link-light mr-2">
+            <h5 className="mt-0 ">Columns with stretched link</h5>
+            <p className=" ">
+              Another instance of placeholder content for this other custom
+              component. It is intended to mimic what some real-world content
+              would look like, and we're using it here to give the component a
+              bit of body and size.
+            </p>
+            <a href="#" className="stretched-link">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </aside>
     </>
   );
 };
